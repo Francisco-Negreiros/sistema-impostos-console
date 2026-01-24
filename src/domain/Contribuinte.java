2069f9a38c84c38cf1a51ca8af8f15913da1f788
francisco-negreiros <negreiros.fv@gmail.com>
2026-01-24 16:12:46 -0300

    chore: estrutura inicial do sistema de impostos

---
+++ src/domain/Contribuinte.java
@@ -0,0 +1,23 @@
+package domain;
+
+public abstract class Contribuinte {
+
+    private String nome;
+    private double rendaAnual;
+
+    public Contribuinte(String nome, double rendaAnual) {
+        this.nome = nome;
+        this.rendaAnual = rendaAnual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getRendaAnual() {
+        return rendaAnual;
+    }
+
+    public abstract double calcularImposto();
+}
+
